3e1af1a07f33a38af9b98286aab82aa06acb27d2
Dev Maktub <brenoluiz1@gmail.com>
2021-10-03 21:46:17 -0300

    Initial commit

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'Default',
+    description: 'Default'
+  }
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+
+export const Default: Story = (args) => <Main {...args} />
